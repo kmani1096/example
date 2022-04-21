437513d442ea4da5e549f2b9c00ba7bf2d3c14ec
ChandraM6 <99394161+ChandraM6@users.noreply.github.com>
2022-04-21 23:17:39 +0100

    Update start-process.sh

--- scripts/start-process.sh
+++ scripts/start-process.sh
@@ -1,2 +1,4 @@
-#!/bin/bash
-java -jar /tmp/airline.war > /dev/null 2> /dev/null < /dev/null &
+#!/usr/bin/env bash
+cd /home/ec2-user/server
+sudo /usr/bin/java -jar -Dserver.port=portnum  
+   *.war > /dev/null 2> /dev/null < /dev/null &
